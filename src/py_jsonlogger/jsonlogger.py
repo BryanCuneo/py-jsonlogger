3a833030ef50546ee6041d2c2cedc6f38ca18822
Bryan Cuneo <bryan.cuneo@gmail.com>
2026-08-05 10:04:42 -0500

    Added LogEntry class.

--- src/py_jsonlogger/jsonlogger.py
+++ src/py_jsonlogger/jsonlogger.py
@@ -1,45 +1,48 @@
-from dataclasses import dataclass
+import inspect
 import json
-import sys
-from datetime import datetime, timezone
+import platform
+import traceback
+from datetime import UTC, datetime, timezone
 from enum import IntEnum, StrEnum
 from pathlib import Path
-from typing import Optional
 
 
 class Levels(IntEnum):
-    DEBUG    = 10
-    INFO     = 20
-    SUCCESS  = 25
-    WARNING  = 30
-    ERROR    = 40
-    FATAL    = 50
-    VERBOSE  = 60
+    DEBUG = 10
+    INFO = 20
+    SUCCESS = 25
+    WARNING = 30
+    ERROR = 40
+    FATAL = 50
+    VERBOSE = 60
+
 
 class ConsoleStyles(StrEnum):
-    SIMPLE    = "simple"
-    TIMESPAN  = "timespan"
+    SIMPLE = "simple"
+    TIMESPAN = "timespan"
     TIMESTAMP = "timestamp"
 
+
 _Loggers = {}
+
 
 class Logger:
     JsonLoggerVersion = "1.3.0a"
 
     def __init__(
-            self,
-            path: str,
-            programName: str,
-            printToConsole: ConsoleStyles | str = None,
-            encoding: str = "utf-8",
-            overwrite = False) -> None:
-        
-        self.start_time = datetime.now(timezone.utc).astimezone().isoformat()
+        self,
+        path: str,
+        programName: str,
+        printToConsole: ConsoleStyles | str | None = None,
+        encoding: str = "utf-8",
+        overwrite=False,
+    ) -> None:
+
+        self.start_time = datetime.now(UTC).astimezone().isoformat()
         self.path = Path(path)
         self.program_name = programName
         self.encoding = encoding
         self.overwrite = overwrite
-
 
         if isinstance(printToConsole, str):
             printToConsole = printToConsole.lower()
@@ -47,21 +50,25 @@ class Logger:
         try:
             self.print_to_console = ConsoleStyles(printToConsole)
         except ValueError:
-            raise ValueError(f"Invalid console style: {printToConsole}. Allowed: {[s.value for s in ConsoleStyles]}") from None
+            raise ValueError(
+                f"Invalid console style: {printToConsole}. Allowed: {[s.value for s in ConsoleStyles]}"
+            ) from None
 
-        if (self.overwrite or not self.path.is_file()):
-            f = open(self.path, "w", encoding=self.encoding)
-            f.close()
-        elif(self.path.is_file()):
-            raise FileExistsError(f"The file {self.path} already exists. use overwrite=True to overwrite it.")
-        elif(self.path.exists() and not self.path.is_file()):
+        if self.overwrite or not self.path.is_file():
+            with open(self.path, "w", encoding=self.encoding):
+                pass
+        elif self.path.is_file():
+            raise FileExistsError(
+                f"The file {self.path} already exists. use overwrite=True to overwrite it."
+            )
+        elif self.path.exists() and not self.path.is_file():
             raise IsADirectoryError(f"The file {self.path} is not a valid file.")
 
         initial_entry: dict[str, str] = {
             "timestamp": self.start_time,
             "level": "START",
             "programName": self.program_name,
-            "pythonVersion": sys.version,
+            "pythonVersion": platform.python_version(),
             "jsonLoggerVersion": self.JsonLoggerVersion,
         }
 
@@ -71,15 +78,63 @@ class Logger:
                 f.write(initial_entry_json)
 
             if self.print_to_console is not None:
-                print(f"{initial_entry["level"]} {datetime.fromisoformat(self.start_time).strftime('%Y-%m-%d %H:%M:%S')} {self.program_name}")
-        except Exception as e:
+                print(
+                    f"{initial_entry['level']} {datetime.fromisoformat(self.start_time).strftime('%Y-%m-%d %H:%M:%S')} {self.program_name}"
+                )
+        except Exception as e:  # noqa: BLE001
             print(f"Error writing initial log entry: {e}")
 
-    def _add_to_initial_entry(newFieldName: str, value) -> None:
+    def _add_to_initial_entry(self, newFieldName: str, value) -> None:
         pass
 
-    def _log(level: Levels, message: str, called_from: str, context: list[object], include_call_stack: bool) -> None:
+    def _log(
+        self,
+        level: Levels,
+        message: str,
+        called_from: str,
+        context: list[object],
+        include_call_stack: bool,
+    ) -> None:
         pass
 
-    def close(message: str = None) -> None:
+    def close(self, message: str | None = None) -> None:
         pass
+
+
+class LogEntry:
+    def __init__(
+        self,
+        level: Levels,
+        message: str,
+        called_from: str,
+        context: list[object] | None = None,
+        include_call_stack: bool = False,
+    ) -> None:
+        self.timestamp: str = datetime.now(UTC).astimezone().isoformat()
+        self.level: Levels = level
+        self.message: str = message
+        self.called_from: str = called_from
+
+        try:
+            self.called_from: str = inspect.currentframe().f_back.f_code.co_name  # type: ignore
+        except AttributeError:
+            self.called_from = "unknown"
+
+        if context is not None or not []:
+            self.context = context
+
+        if include_call_stack or self.level in [Levels.FATAL, Levels.VERBOSE]:
+            self.call_stack = traceback.extract_stack()
+        else:
+            self.call_stack = None
+
+    def __str__(self) -> str:
+        str = f"Timestamp:\n\t{self.timestamp}\nLevel:\n\t{self.level.name}\nMessage:\n\t{self.message}\nCalled From:\n\t{self.called_from}\n"
+        if self.context is not None:
+            str += f"Context:\n\t{self.context}\n"
+        if self.call_stack is not None:
+            str += f"Call Stack:\n\t{self.call_stack}\n"
+        return str
+
+    def __repr__(self) -> str:
+        return self.__str__()
--- tests/test_logs.py
+++ tests/test_logs.py
@@ -1,6 +1,14 @@
 from py_jsonlogger import *
 
-#def test_logs():
+# def test_logs():
 #    logger = Logger(path="test.log", programName="test", printToConsole="simple", overwrite=True)
 
-logger = Logger(path="test.log", programName="test", printToConsole=ConsoleStyles.SIMPLE, overwrite=True)
+logger = Logger(
+    path="test.log",
+    programName="test",
+    printToConsole=ConsoleStyles.SIMPLE,
+    overwrite=True,
+)
+
+log_entry = LogEntry(level=Levels.DEBUG, message="This is a test", called_from="test")
+print(log_entry)
